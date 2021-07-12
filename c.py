0f611a6a73bd7e2f291b274398295fd9c165dbbe
Zuck3r <51792327+Zuck3-r@users.noreply.github.com>
2021-07-12 16:50:26 +0900

    通知を30分に一回, 余計な出力の削除

--- c.py
+++ c.py
@@ -7,8 +7,7 @@ target_port=1234
 client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
 while True:
-    time.sleep(10)
+    time.sleep(1800)
     data = str(datetime.datetime.now()) + "\n"
     client.send(data.encode('utf-8'))
-    print("waiting for her response...")
 
